3226349db033d6ebc1bf43bfc08f01fbcd3cae20
Aaron Speer <ajamesspeer@gmail.com>
2018-04-16 17:26:21 +0100

    Finalise config example

--- config/config.example.php
+++ config/config.example.php
@@ -1,2 +1,8 @@
-printers[PDF] = "Print to PDF"
-printers[CPL-320] = "Study CLP-320"
+appname = "Conference Name"
+
+printers[cups-code] = "Description of printer"
+printers[...] = "..."
+
+
+step2instructions = "If you have a .pages (Apple Pages), or more complex Word file (i.e. Formatted Notepaper) please export it as a PDF, or use the public PCs on the 2nd floor"
+failedinstructions = "Try uploading a PDF, or use the public PCs on the 2nd Floor"
